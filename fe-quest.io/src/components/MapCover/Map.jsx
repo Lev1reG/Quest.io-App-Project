1de558ed0c6038ae2aaadd0084bea08de04b7b40
eben4ya <benaya.imanuela@gmail.com>
2023-12-20 15:51:55 +0700

    <fix: layouting map-cover>

--- fe-quest.io/src/components/MapCover/Map.jsx
+++ fe-quest.io/src/components/MapCover/Map.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Item from "./Item";
-import Quote from "./Quote";
 import ItemList from "./ItemList";
 
 const Map = ({ openSection }) => {
   return (
-    <div className="absolute w-[81.77%] aspect-[1570/964] flex justify-center items-center">
+    <div className="absolute h-[89.26vh] aspect-[1570/964] flex justify-center items-center">
       <div
         style={{
           background: "url(/MapCover/bg-Land.png)",
@@ -14,7 +12,7 @@ const Map = ({ openSection }) => {
         className="absolute bottom-0 left-0 w-[81.77%] aspect-[1570/964] bg-no-repeat"
       >
         <ItemList
-          parentClass="absolute top-0 left-0 w-[190px] aspect-[195/375]"
+          parentClass="absolute top-0 left-0 w-[171px] aspect-[195/375]"
           itemProperty={{
             onClick: () => {
               alert("Feature is not available yet");
@@ -22,20 +20,20 @@ const Map = ({ openSection }) => {
             src: "/MapCover/tower.png",
             alt: "Tower",
             parentClass:
-              "absolute bottom-0 right-0 w-[70px] aspect-[100/423.28] ",
+              "absolute bottom-0 right-0 w-[63px] aspect-[100/423.28] ",
             childClass:
               "absolute object-contain hover:scale-105 hover:brightness-125 transition-all duration-300 ease-in-out",
           }}
           quoteProperty={{
             parentClass:
-              "absolute top-[7%] -right-1/2 flex justify-center items-center w-[181px] aspect-[362/176]",
+              "absolute top-[2%] -right-[55%] flex justify-center items-center w-[181px] aspect-[362/176]",
             childClass:
               "font-PixelifySans max-w-[140px] h-fit text-black ml-4 mb-2 text-[15px] font-normal leading-none",
             text: "Still Working on it!",
           }}
         />
         <ItemList
-          parentClass="absolute top-[6.7%] left-[43.8%] w-[245px] aspect-[410/365]"
+          parentClass="absolute top-[4.7%] left-[43%] w-[245px] aspect-[410/365]"
           itemProperty={{
             onClick: openSection,
             src: "/MapCover/house.png",
@@ -62,13 +60,13 @@ const Map = ({ openSection }) => {
             src: "/MapCover/AngelFigure.png",
             alt: "Statue",
             parentClass:
-              "absolute top-[25%] right-[39%] w-[114.8px] aspect-[164/153]",
+              "absolute top-[20%] right-[33%] w-[114.8px] aspect-[164/153]",
             childClass:
               "absolute object-contain hover:scale-105 hover:brightness-125 transition-all duration-300 ease-in-out",
           }}
           quoteProperty={{
             parentClass:
-              "absolute -top-[28%] -right-[15%] flex justify-center items-center w-[181px] aspect-[362/176] ",
+              "absolute -top-[30%] -right-[15%] flex justify-center items-center w-[181px] aspect-[362/176] ",
             childClass:
               "font-PixelifySans max-w-[140px] h-fit text-black ml-5 mb-2 text-[15px] font-normal leading-none ",
             text: "Click here to see your rewards!",
